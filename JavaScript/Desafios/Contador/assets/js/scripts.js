2fc4147ca745d660c89e71ffe51ecc86229d1385
Willjpg <willianf.silva@outlook.com>
2022-06-13 23:55:14 -0300

    Desafio: contador

--- JavaScript/Desafios/Contador/assets/js/scripts.js
+++ JavaScript/Desafios/Contador/assets/js/scripts.js
@@ -5,28 +5,45 @@ const CURRENT_NUMBER = document.getElementById('currentNumber');
 function increment(){
     count++;
     CURRENT_NUMBER.innerHTML = count;
+    numRed();
+    numTime();
 }
 
 function decrement(){
     count--;
     CURRENT_NUMBER.innerHTML = count;
-
-   
+    numRed();
+   numTime();
 }
 
 function reset(){
     count=0;
     CURRENT_NUMBER.innerHTML = count;
+    numRed();
 }
-// function numRed(){
-//     if(CURRENT_NUMBER<0){
-//         CURRENT_NUMBER.style.color ='red'
-//     }
-// }
 
+
+
+function numRed(){ 
+if(count<0){
+    CURRENT_NUMBER.style.color = 'red'
+}else if(count>0){
+    CURRENT_NUMBER.style.color = '#ccff33'
+}else{
+    CURRENT_NUMBER.style.color = 'white'
+}
+}
+
+function numTime(){
+    if(count>=10){
+        count=0
+    }
+
+
+}
 
 function addEventos(){//substitui o onClick dentro  do Index
-    // document.getElementById("CURRENT_NUMBER").addEventListener("click", numRed)
+    // document.getElementById("CURRENT_NUMBER")
     document.getElementById("adicionar").addEventListener("click", increment )
     document.getElementById("subtrair").addEventListener
     ("click", decrement )
