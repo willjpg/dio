add6ddba42403c2fadb9649a8b2877355189d9e5
willjpg <willianf.silva@outlook.com>
2022-07-11 22:16:19 -0300

    Desenvolvendo e testando o método post

--- NodeJs/ARQREST/src/index.ts
+++ NodeJs/ARQREST/src/index.ts
@@ -3,7 +3,11 @@ import usersRoute from './routes/users.route';
 
 
 const app = express();
+// Configurações da Aplicação 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
+// Configuração de Rotas
 app.use(usersRoute);
 
 
@@ -12,6 +16,7 @@ app.get('/status',(req: Request, res: Response, next:NextFunction)=> {
 
 });
 
+// Inicialização do servidor
 app.listen(3000,()=>{
     console.log('Aplicação executando na porta 3000!')
 });
--- NodeJs/ARQREST/src/routes/users.route.ts
+++ NodeJs/ARQREST/src/routes/users.route.ts
@@ -20,9 +20,13 @@ usersRoute.get('/users', (req: Request, res: Response, next:NextFunction) =>{
 
 usersRoute.get('/users/:uuid', (req: Request<{ uuid: string}>, res: Response, next:NextFunction) =>{
     const uuid = req.params.uuid;
-    
     res.status(StatusCodes.OK).send({uuid});
 })
+
+usersRoute.post('/users', (req: Request, res: Response, next:NextFunction) =>{
+    const newUser = req.body;
+    res.status(StatusCodes.CREATED).send(newUser);
+});
 
 export default usersRoute;
 
