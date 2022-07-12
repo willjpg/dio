712d686b8147db63fd6d5ce217274d511b8cca51
willjpg <willianf.silva@outlook.com>
2022-07-12 13:43:50 -0300

    Criando a estrutura básica para conexão com banco de dados

--- NodeJs/ARQREST/src/db.ts
+++ NodeJs/ARQREST/src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 
-const connectionString = ``;
+const connectionString = `postgres://vgwxdklc:2iCdwcMGvT9QdGlmac9nXaDR09CmC0d4@kesavan.db.elephantsql.com/vgwxdklc`;
 
 const db = new Pool({ connectionString});
 
---
+++ NodeJs/ARQREST/src/repositories/user.repository.ts
@@ -0,0 +1,12 @@
+import User from "../models/user.model";
+
+class UserRepository{
+
+    findAllUsers(): User[]{
+        return[];
+    }
+
+
+}
+
+export default new UserRepository();
