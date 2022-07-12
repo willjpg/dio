4382ecd9fb493264bc851e1b5f4967a5350a689c
willjpg <willianf.silva@outlook.com>
2022-07-11 21:22:08 -0300

    Adicionando o typescript e incrementando a forma de desenvolver

--- NodeJs/ARQREST/src/index.ts
+++ NodeJs/ARQREST/src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello world!')
+import express, {Request, Response, NextFunction} from 'express';
+
+
+const app = express();
+
+app.get('/status',(req: Request, res: Response, next:NextFunction)=> { 
+    res.status(200).send({ foo: 'bar do juninho'});
+
+});
+
+app.listen(3000,()=>{
+    console.log('Aplicação executando na porta 3000!')
+});
